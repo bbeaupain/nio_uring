ebc4a6e57b7070fa828da00064134638878e65b3
Paul deGrandis <paul.degrandis@gmail.com>
2024-08-29 13:29:53 -0400

    Include a test for seeking through a file

--- src/test/java/sh/blake/niouring/IoUringFileTest.java
+++ src/test/java/sh/blake/niouring/IoUringFileTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IoUringFileTest extends TestBase {
 
@@ -31,5 +32,38 @@ public class IoUringFileTest extends TestBase {
         attemptUntil(ioUring::execute, readSuccessfully::get);
 
         Assert.assertTrue("File read successfully", readSuccessfully.get());
+    }
+
+    @Test
+    public void seek_read_readme_should_succeed() {
+        // This test is fragile and will need to be maintained
+        // as the README is updated.
+        String fileName = "README.md";
+        AtomicInteger readCounter = new AtomicInteger(0);
+        AtomicBoolean seekSuccessfully = new AtomicBoolean(false);
+        int chunkSize = 1024;
+        ByteBuffer buffer = ByteBuffer.allocateDirect(chunkSize);
+
+        IoUring ioUring = new IoUring(TEST_RING_SIZE)
+            .onException(Exception::printStackTrace);
+
+        IoUringFile file = new IoUringFile(fileName);
+        file.onRead(in -> {
+            in.flip();
+            int count = readCounter.incrementAndGet();
+            String fileStr = StandardCharsets.UTF_8.decode(in).toString();
+
+            if (count == 1) {
+                in.clear(); // reuse the buffer provided
+                ioUring.queueRead(file, in, chunkSize); // read with offset
+            } else if (fileStr.startsWith("et = new IoUringServerSocket(8080);")) {
+                seekSuccessfully.set(true);
+            }
+        });
+
+        ioUring.queueRead(file, buffer);
+        attemptUntil(ioUring::execute, seekSuccessfully::get);
+
+        Assert.assertTrue("File seek successfully", seekSuccessfully.get());
     }
 }
